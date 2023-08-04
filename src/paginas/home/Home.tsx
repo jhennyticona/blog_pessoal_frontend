320d6a59916be370a34a6d2fcd3e567843306c32
jhennyticona <102036237+jhennyticona@users.noreply.github.com>
2023-08-04 12:40:14 -0300

    implementação alerts personalizados

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 import homeLogo from '../../assets/home.png'
 import './Home.css';
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
@@ -13,11 +14,12 @@ function Home() {
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opiniões</p>
   
               <div className="flex justify-around gap-4">
               <ModalPostagem />
-              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              <button className='rounded bg-white text-blue-800 py-2 px-4'><Link to ="/postagens">Ver postagens</Link></button>
+          
             </div>
             </div>
   
